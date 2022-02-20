dcf1a135bb389a31c37a8927b093f4af14ebf584
Kelvin Elsendoorn <Kelvin Elsendoorn>
2022-02-20 20:38:09 +0100

    Epoll - cleanup

--- src/event_handlers/epoll.c
+++ src/event_handlers/epoll.c
@@ -14,7 +14,6 @@
 int (*dsu_epoll_wait)(int, struct epoll_event *, int, int);
 int (*dsu_epoll_create1)(int);
 int (*dsu_epoll_create)(int);
-//int (*dsu_epoll_ctl)(int, int, int, struct epoll_event *);
 
 
 int dsu_live_epoll = 0;
@@ -31,33 +30,6 @@ int epoll_create(int size) {
 	DSU_DEBUG_PRINT(" Epoll_create() (%d)\n", (int) getpid());
 	return dsu_epoll_create(size);
 }
-
-
-//int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event) {
-//	DSU_DEBUG_PRINT(" Epoll_ctl(%d, %d, %d, event) (%d)\n", epfd, op, fd, (int) getpid());
-	/* 	This system call is used to add, modify, or remove entries in the interest list. Because this list is not accessible from user 
-		space, the event must be stored for binded sockets. It wil be used in the dsu_post_epoll call to restore the original event 
-		settings. */
-
-//	struct dsu_socket_list *dsu_sockfd = dsu_sockets_search_fd(dsu_program_state.binds, fd);
-	
-
-//	if (dsu_sockfd == NULL)	return dsu_epoll_ctl(epfd, op, fd, event);
-	
-	
-//	if (op == EPOLL_CTL_ADD || op == EPOLL_CTL_MOD) {
-//		DSU_DEBUG_PRINT(" - Store (%d)\n", (int) getpid());
-//		memcpy(&dsu_sockfd->ev, event, sizeof(struct epoll_event));
-//	} else {
-//		DSU_DEBUG_PRINT(" - Delete (%d)\n", (int) getpid());
-//		memset(&dsu_sockfd->ev, 0, sizeof(struct epoll_event));
-//	}
-
-
-//	return dsu_epoll_ctl(epfd, op, dsu_sockfd->shadowfd, event);
-//}
-
-
 
 
 void dsu_epoll_internal_conn(struct dsu_socket_list *dsu_sockfd, int epollfd) {
@@ -245,103 +217,14 @@ void dsu_handle_conn_epoll(struct dsu_socket_list *dsu_sockfd, int epollfd, int 
 }
 
 
-//void dsu_epoll_shadowfd(struct dsu_socket_list *dsu_sockfd, int epollfd) {
-	
-
-	/*	Deactivate socket. */
-	//if (dsu_sockfd->ev.events != 0) {
-	//	DSU_DEBUG_PRINT(" - remove %d (%d)\n", dsu_sockfd->fd, (int) getpid());
-	//	dsu_epoll_ctl(epollfd, EPOLL_CTL_DEL, dsu_sockfd->fd, NULL);
-	//}
-	
-
-	/* 	Set shadow file descriptor if needed... */
-//	struct epoll_event ev;
-//	memcpy(&ev, &dsu_sockfd->ev, sizeof(struct epoll_event));
- //   ev.data.fd = dsu_sockfd->shadowfd; // Overwrite union ptr, fd, uint32, uint64 to shadow file descriptor.
-	
-
-//	if (dsu_sockfd->monitoring && dsu_sockfd->ev.events != 0) { 
-		
-
-//		DSU_DEBUG_PRINT(" < Lock status %d (%d)\n", dsu_sockfd->port, (int) getpid());
-//		if (sem_wait(dsu_sockfd->status_sem) == 0) {
-            
-
-            /*  Only one process can monitor a blocking socket. During transfer use lock. */
-//			if (dsu_sockfd->status[DSU_TRANSFER] > 0 && dsu_sockfd->blocking) {
-				
-                
-//				DSU_DEBUG_PRINT(" - Try lock fd %d (%d)\n", dsu_sockfd->port, (int) getpid());
-//				if (sem_trywait(dsu_sockfd->fd_sem) == 0) {
-//					
-
-//					DSU_DEBUG_PRINT(" < Lock fd %d (%d)\n", dsu_sockfd->port, (int) getpid());
-//					dsu_sockfd->locked = DSU_LOCKED;
-
-
-//					DSU_DEBUG_PRINT(" - Set %d => %d (%d)\n", dsu_sockfd->fd, dsu_sockfd->shadowfd, (int) getpid());
-//					dsu_epoll_ctl(epollfd, EPOLL_CTL_ADD, dsu_sockfd->shadowfd, &ev);
-
-//				}
-
-//			} else {
-
-				
-//				DSU_DEBUG_PRINT(" - Set %d => %d (%d)\n", dsu_sockfd->fd, dsu_sockfd->shadowfd, (int) getpid());
-//				dsu_epoll_ctl(epollfd, EPOLL_CTL_ADD, dsu_sockfd->shadowfd, &ev);
-
-//			}
-
-						
-
-//			DSU_DEBUG_PRINT(" > Unlock status %d (%d)\n", dsu_sockfd->port, (int) getpid());
-//			sem_post(dsu_sockfd->status_sem);
-//		}
-//	}	
-//}
-
-
 void dsu_pre_epoll(int epollfd) {
 
 	
-	/*	Set shadow file descriptors. */
-	//dsu_forall_sockets(dsu_program_state.binds, dsu_epoll_shadowfd, epollfd);
-
-
 	/*	Sniff on internal connections. */
 	dsu_forall_sockets(dsu_program_state.binds, dsu_epoll_internal_conn, epollfd);
 		
 	
 }
-
-
-//void dsu_epoll_unlock(struct dsu_socket_list *dsu_sockfd) {
-//
-//
-//	if (dsu_sockfd->locked == DSU_LOCKED) {
-//       DSU_DEBUG_PRINT(" > Unlock fd %d (%d)\n", dsu_sockfd->port, (int) getpid());
-//        sem_post(dsu_sockfd->fd_sem);
-//        dsu_sockfd->locked = DSU_UNLOCKED;
-//    }
-
-	
-//}
-
-
-//void dsu_epoll_originalfd(struct dsu_socket_list *dsu_sockfd, int epollfd) {
-
-	/*	To avoid changing the external behaviour, restore the epoll settings. */
-//	if (dsu_sockfd->ev.events != 0) {
-//		struct epoll_event ev;
-//		memcpy(&ev, &dsu_sockfd->ev, sizeof(struct epoll_event));
-
-//		DSU_DEBUG_PRINT(" - %d => %d (%d)\n", dsu_sockfd->shadowfd, dsu_sockfd->fd, (int) getpid());
-//		dsu_epoll_ctl(epollfd, EPOLL_CTL_DEL, dsu_sockfd->shadowfd, NULL);
-//		dsu_epoll_ctl(epollfd, EPOLL_CTL_ADD, dsu_sockfd->fd, &ev);
-//	}
-
-//}
 
 
 int dsu_epoll_correct_events(int nfds, struct epoll_event *events) {
@@ -363,23 +246,9 @@ int dsu_epoll_correct_events(int nfds, struct epoll_event *events) {
 
 		} 
 
-		
-		/*	Change shadow file descripter back to the original file descriptor. Because struct epoll_event is an union of a pointer, 
-			file descriptor or an array index we have to use the stored struct epoll_event (See epoll_ctl). */
-		//else {
-
-		//	struct dsu_socket_list *dsu_sockfd = dsu_sockets_search_shadowfd(dsu_program_state.binds, events[i].data.fd);
-		//	if (dsu_sockfd != NULL && dsu_sockfd->ev.events != 0) {
-				
-		//		uint32_t _events = events[i].events;
-		//		memcpy(&events[i], &dsu_sockfd->ev, sizeof(struct epoll_event));
-		//		events[i].events = _events; // Overwrite the initial event setting with the response of epoll_wait.
-			
-		//	}
-		//}
-
-		
+	
 	}
+	
 	
 	return nfds;
 }
@@ -415,10 +284,6 @@ int dsu_post_epoll(int epollfd, int nfds, struct epoll_event *events) {
 
 	/*  Handle internal messages. */ 
     dsu_forall_sockets(dsu_program_state.binds, dsu_handle_conn_epoll, epollfd, nfds, events);
-
-	
-	/*	Unlock binded file descriptors if needed. */
-	//dsu_forall_sockets(dsu_program_state.binds, dsu_epoll_unlock);
 	
 	
 	/*  Change shadow file descripter to its original file descriptor in epoll. */
@@ -458,7 +323,13 @@ int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
 
 	/*	Translate epoll response. */
 	nfds = dsu_post_epoll(epfd, nfds, events);
-
+	
+	
+	/* Avoid changing external behaviour. Most applications cannot handle return value of 0, hence call select recursively. */
+	if (nfds == 0 && timeout < 0) {
+		return epoll_wait(epfd, events, maxevents, timeout);
+	}
+	
 
 	return nfds;
 }
