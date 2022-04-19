73bc931630c1b6b0afeab4a213c75cca60c8ecfe
Kelvin Elsendoorn <Kelvin Elsendoorn>
2022-04-19 21:21:20 +0200

    Update benchmarking

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -10,6 +10,7 @@ import re
 
 comparison = False
 round_robin = False
+comparison_update = False
 
 full_path = os.path.realpath(__file__)
 path, filename = os.path.split(full_path)
@@ -17,18 +18,14 @@ path, filename = os.path.split(full_path)
 webservers = ["nginx", "apache", "lighttpd"]
 
 def start_nginx():
-	subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=10000", "openDSU"])
-	subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "openDSU", "./nginx.o"])
+	subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./nginx.o"])
 	
 def start_lighttpd():
-	subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=5000", "openDSU"])
-	#subprocess.run(["openDSU", "./lighttpd.o", "-f", path + "/config/lighttpd.conf"])
-	
-	subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "openDSU", "./lighttpd.o -f " + path + "/config/lighttpd.conf"])
+	subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./lighttpd.o -f " + path + "/config/lighttpd.conf"])
 
 def start_apache():
-	subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=20000", "openDSU"])
-	subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "openDSU", "./apache.o -f " + path + "/config/apache.conf"])
+	subprocess.run(["rm", "etc/apache/logs/httpd.pid"])
+	subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./apache.o -f " + path + "/config/apache.conf"])
 
 def start_web_server(name):
 	if name == "nginx":
@@ -44,13 +41,16 @@ def terminate_web_servers():
 		subprocess.run(["pkill", webserver])
 
 def start_apache_benchmark(f):
-	return subprocess.Popen(["ab", "-g", f, "-n", str(REQUESTS), "-H", "Connection: close", "-s", "60", "-c", "32", "http://localhost/v1.html"])
+	return subprocess.Popen(["ab", "-g", f, "-n", str(REQUESTS), "-H", "Connection: close", "-s", "10", "-c", "8", "http://localhost/v1.html"])
 
 def start_wrk_benchmark(f):
-	subprocess.run(["./wrk.o", "-t", "8", "-c", "64", "-d", "30s", "http://localhost/v1.html"], stdout=f)
+	subprocess.run(["./wrk.o", "-t", "8", "-c", "64", "-d", "180s", "http://localhost/v1.html"], stdout=f)
+
+def start_wrk_benchmark_async(f):
+	return subprocess.Popen(["./wrk.o", "-t", "8", "-c", "64", "-d", "60s", "http://localhost/v1.html"], stdout=f)
 
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "cr", ["run"])
+	opts, args = getopt.getopt(sys.argv[1:], "cru", ["run"])
 except getopt.GetoptError as err:
 	print(err)
 	sys.exit(2)
@@ -61,6 +61,8 @@ for o, a in opts:
 		comparison = True
 	if o == "-r":
 		round_robin = True
+	if o == "-u":
+		comparison_update = True
 
 
 if comparison:
@@ -70,31 +72,44 @@ if comparison:
 	
 	with open("comparison.txt", "w") as f:
 
-		print("CPU bounded")
-		
-		print("Start nginx")
-		f.write("Nginx\n")
-		f.flush()
-		subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=5000", "openDSU"])
-		subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "./nginx.o"])
-		start_wrk_benchmark(f)
-		terminate_web_servers()
-		time.sleep(WAIT_COMP)
+		print("CPU bounded 1 thread")
+		subprocess.run(["cgset", "-r", "cpuset.cpus=1-4", "openDSU"])
+		subprocess.run(["cgset", "-r", "cpuset.mems=0", "openDSU"])
 
 		print("Start openDSU nginx")
 		f.write("openDSU nginx\n")
 		f.flush()
-		subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=5000", "openDSU"])
-		subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "openDSU", "./nginx.o"])
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./nginx.o"])
 		start_wrk_benchmark(f)
 		terminate_web_servers()
 		time.sleep(WAIT_COMP)
 
-		print("Start lighttpd")
-		f.write("lighttpd\n")
+		print("Start nginx")
+		f.write("Nginx\n")
 		f.flush()
-		subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=2500", "openDSU"])
-		subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "./lighttpd.o", "-f", "config/lighttpd.conf"])
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "./nginx.o"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		sys.exit(0)
+
+		print("CPU bounded 1 thread")
+		subprocess.run(["cgset", "-r", "cpuset.cpus=1", "openDSU"])
+		subprocess.run(["cgset", "-r", "cpuset.mems=0", "openDSU"])
+
+		print("Start openDSU nginx")
+		f.write("openDSU nginx\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./nginx.o"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		print("Start nginx")
+		f.write("Nginx\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "./nginx.o"])
 		start_wrk_benchmark(f)
 		terminate_web_servers()
 		time.sleep(WAIT_COMP)
@@ -102,30 +117,7 @@ if comparison:
 		print("Start openDSU lighttpd")
 		f.write("openDSU lighttpd\n")
 		f.flush()
-		subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=2500", "openDSU"])
-		subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "openDSU", "./lighttpd.o -f config/lighttpd.conf"])
-		start_wrk_benchmark(f)
-		terminate_web_servers()
-		time.sleep(WAIT_COMP)
-
-		print("Memory bounded")
-		
-		print("start nginx")
-		f.write("nginx\n")
-		f.flush()
-		subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=-1", "openDSU"])
-		subprocess.run(["cgset", "-r", "memory.limit_in_bytes=100000000", "openDSU"])
-		subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "./nginx.o"])
-		start_wrk_benchmark(f)
-		terminate_web_servers()
-		time.sleep(WAIT_COMP)
-
-		print("Start openDSU nginx")
-		f.write("openDSU nginx\n")
-		f.flush()
-		subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=-1", "openDSU"])
-		subprocess.run(["cgset", "-r", "memory.limit_in_bytes=100000000", "openDSU"])
-		subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "openDSU", "./nginx.o"])
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./lighttpd.o -f config/lighttpd.conf"])
 		start_wrk_benchmark(f)
 		terminate_web_servers()
 		time.sleep(WAIT_COMP)
@@ -133,26 +125,198 @@ if comparison:
 		print("Start lighttpd")
 		f.write("lighttpd\n")
 		f.flush()
-		subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=-1", "openDSU"])
-		subprocess.run(["cgset", "-r", "memory.limit_in_bytes=100000000", "openDSU"])
-		subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "./lighttpd.o", "-f", "config/lighttpd.conf"])
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "./lighttpd.o", "-f", "config/lighttpd.conf"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		print("Start openDSU apache")
+		f.write("openDSU apache\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./apache.o -f ../../config/apache.conf"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		print("Start apache")
+		f.write("apache\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "./apache.o", "-f", "../../config/apache.conf"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		print("CPU bounded 2 threads")
+		subprocess.run(["cgset", "-r", "cpuset.cpus=1-2", "openDSU"])
+
+		print("Start openDSU nginx")
+		f.write("openDSU nginx\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./nginx.o"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		print("Start nginx")
+		f.write("Nginx\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "./nginx.o"])
 		start_wrk_benchmark(f)
 		terminate_web_servers()
 		time.sleep(WAIT_COMP)
 
 		print("Start openDSU lighttpd")
-		f.write("lighttpd\n")
+		f.write("openDSU lighttpd\n")
 		f.flush()
-		subprocess.run(["cgset", "-r", "cpu.cfs_quota_us=-1", "openDSU"])
-		subprocess.run(["cgset", "-r", "memory.limit_in_bytes=100000000", "openDSU"])
-		subprocess.run(["cgexec", "-g", "cpu,memory:openDSU", "openDSU", "./lighttpd.o -f config/lighttpd.conf"])
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./lighttpd.o -f config/lighttpd.conf"])
 		start_wrk_benchmark(f)
 		terminate_web_servers()
 		time.sleep(WAIT_COMP)
 
+		print("Start lighttpd")
+		f.write("lighttpd\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "./lighttpd.o", "-f", "config/lighttpd.conf"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		print("Start openDSU apache")
+		f.write("openDSU apache\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./apache.o -f ../../config/apache.conf"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		print("Start apache")
+		f.write("apache\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "./apache.o", "-f", "../../config/apache.conf"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		print("CPU bounded 4 threads")
+		subprocess.run(["cgset", "-r", "cpuset.cpus=1-4", "openDSU"])
+
+		print("Start openDSU apache")
+		f.write("openDSU apache\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./apache.o -f ../../config/apache.conf"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		print("Start apache")
+		f.write("apache\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "./apache.o", "-f", "../../config/apache.conf"])
+		start_wrk_benchmark(f)
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+if comparison_update:
+	
+	WAIT_COMP = 15
+	terminate_web_servers()
+	
+	with open("comparison_update.txt", "w") as f:
+
+		subprocess.run(["cgset", "-r", "cpuset.cpus=1-4", "openDSU"])
+		subprocess.run(["cgset", "-r", "cpuset.mems=0", "openDSU"])
+		
+		print("Start openDSU nginx")
+		f.write("openDSU nginx\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./nginx.o"])
+		p = start_wrk_benchmark_async(f)
+		time.sleep(10)
+		print("Start openDSU nginx")
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./nginx.o"])
+		time.sleep(10)
+		print("Start openDSU nginx")
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./nginx.o"])
+		time.sleep(10)
+		print("Start openDSU nginx")
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./nginx.o"])
+		time.sleep(10)
+		print("Start openDSU nginx")
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./nginx.o"])
+		time.sleep(10)
+		print("Start openDSU nginx")
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./nginx.o"])
+
+		p.wait()
+		terminate_web_servers()
+		sys.exit(0)
+		time.sleep(WAIT_COMP)
+
+		subprocess.run(["cgset", "-r", "cpuset.cpus=1", "openDSU"])
+		subprocess.run(["cgset", "-r", "cpuset.mems=0", "openDSU"])
+
+		print("Start openDSU lighttpd")
+		f.write("openDSU lighttpd\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./lighttpd.o -f config/lighttpd.conf"])
+		p = start_wrk_benchmark_async(f)
+		time.sleep(10)
+		print("Start openDSU lighttpd")
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./lighttpd.o -f config/lighttpd.conf"])
+		time.sleep(10)
+		print("Start openDSU lighttpd")
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./lighttpd.o -f config/lighttpd.conf"])
+		time.sleep(10)
+		print("Start openDSU lighttpd")
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./lighttpd.o -f config/lighttpd.conf"])
+		time.sleep(10)
+		print("Start openDSU lighttpd")
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./lighttpd.o -f config/lighttpd.conf"])
+		time.sleep(10)
+		print("Start openDSU lighttpd")
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./lighttpd.o -f config/lighttpd.conf"])
+
+		p.wait()
+		terminate_web_servers()
+		time.sleep(WAIT_COMP)
+
+		subprocess.run(["cgset", "-r", "cpuset.cpus=1-4", "openDSU"])
+		subprocess.run(["cgset", "-r", "cpuset.mems=0", "openDSU"])
+		
+		print("Start openDSU apache")
+		f.write("openDSU apache\n")
+		f.flush()
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./apache.o -f ../../config/apache.conf"])
+		p = start_wrk_benchmark_async(f)
+		time.sleep(10)
+		print("Start openDSU apache")
+		subprocess.run(["rm", "etc/apache/logs/httpd.pid"])
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./apache.o -f ../../config/apache.conf"])
+		time.sleep(10)
+		print("Start openDSU apache")
+		subprocess.run(["rm", "etc/apache/logs/httpd.pid"])
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./apache.o -f ../../config/apache.conf"])
+		time.sleep(10)
+		print("Start openDSU apache")
+		subprocess.run(["rm", "etc/apache/logs/httpd.pid"])
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./apache.o -f ../../config/apache.conf"])
+		time.sleep(10)
+		print("Start openDSU apache")
+		subprocess.run(["rm", "etc/apache/logs/httpd.pid"])
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./apache.o -f ../../config/apache.conf"])
+		time.sleep(10)
+		print("Start openDSU apache")
+		subprocess.run(["rm", "etc/apache/logs/httpd.pid"])
+		subprocess.run(["cgexec", "-g", "cpuset:openDSU", "--sticky", "openDSU", "./apache.o -f ../../config/apache.conf"])
+
+		p.wait()
+		
+		terminate_web_servers()
+		
 		
 if round_robin:
-	REQUESTS = 5500000
+	#REQUESTS = 6000000
+	REQUESTS = 5000000
 	ROUND_ROBIN_TIME = 30
 
 	#REQUESTS = 500000
@@ -160,6 +324,9 @@ if round_robin:
 	
 		
 	terminate_web_servers()
+
+	subprocess.run(["cgset", "-r", "cpuset.cpus=1", "openDSU"])
+	subprocess.run(["cgset", "-r", "cpuset.mems=0", "openDSU"])
       
 	with open("round_robin_update.plt", "w") as f:
 		f.write("seconds\n")
@@ -255,7 +422,7 @@ if os.path.exists("round_robin1.plt") and os.path.exists("round_robin_update1.pl
 	
 	plt.ylim((0,None))
 	plt.xlim((5,175))
-	plt.ylabel('Troughput in request/second')
+	plt.ylabel('request/second')
 	plt.xlabel('Time in seconds')
 	plt.text(17, 1000, "Nginx")
 	plt.text(42, 1000, "Apache httpd")
--- benchmark/cgroups.sh
+++ benchmark/cgroups.sh
@@ -11,14 +11,22 @@ sudo cgset -r cpu.cfs_quota_us =2500 openDSU
 sudo cat /sys/fs/cgroup/cpu/openDSU/cpu.cfs_period_us
 sudo cat /sys/fs/cgroup/cpu/openDSU/cpu.cfs_quota_us
 sudo cat /sys/fs/cgroup/memory/openDSU/memory.limit_in_bytes
+sudo cat /sys/fs/cgroup/cpuset/openDSU/cpuset.cpus
+echo $$ > /sys/fs/cgroup/cpuset/openDSU/tasks
+
+39211
 
 mkdir /sys/fs/cgroup/cpuset/openDSU
 sudo cgset -r cpuset.cpus=4 openDSU
+sudo cgset -r cpuset.mems=0 openDSU 		bug in cgexec leave mems empty and does not work.
 /sys/fs/cgroup/cpuset/openDSU/cpuset.cpus
 
 sudo cgset -r cpu.cfs_period_us=-1 openDSU
 sudo cgset -r cpu.cfs_quota_us=-1 openDSU
 	
+
+sudo cgexec -g cpuset:openDSU --sticky openDSU ./lighttpd.o -f /config/lighttpd.conf
+ab -n 100000 -H "Connection: close", -s 10 -c 8 http://10.0.0.10/v1.html"]
 #sudo cgclassify -g memory:openDSU 9491
 #sudo ps -o cgroup 12530 | cat
 #sudo cgexec -g cpu,memory:openDSU ./nginx.o
